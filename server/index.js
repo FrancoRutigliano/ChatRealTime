763f40864547a9830779f3fc10a9bdad5b6243cc
FrancoRutigliano <francorutigliano11@gmail.com>
2024-01-25 20:40:41 -0300

    Inicializando index.html ---> estilos, etc

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import logger from 'morgan';
 
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+import { createReadStream } from 'node:fs';
 const port = process.env.port ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', () => {
+    console.log('User has conected')
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    res.send('<h1>Esto es el chat</h1>')
+    // current working directory
+    res.sendFile(process.cwd() + '/client/index.html')
 })
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`)
 })
