109ec144eba0ef7b34cfdca3aa217799564b7d53
FrancoRutigliano <francorutigliano11@gmail.com>
2024-01-30 21:02:50 -0300

    añadiendo conexion con la base de datos turso

--- server/index.js
+++ server/index.js
@@ -1,14 +1,36 @@
 import express from 'express';
 import logger from 'morgan';
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
+//import { DB_TOKEN } from './.env'
 
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
-import { createReadStream } from 'node:fs';
-const port = process.env.port ?? 3000
+
+dotenv.config()
+
+const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    connectionStateRecovery: {}
+})
+
+// conexion a base de datos
+const db = createClient({
+    url: "libsql://valid-old-lace-francorutigliano.turso.io",
+    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIyMDI0LTAxLTI2VDE4OjUyOjMxLjg0MjAyMzQ0NloiLCJpZCI6IjQ1NDE5YTJhLWJjNjYtMTFlZS1hYmZmLWUyZTgyZWFhY2IwNSJ9.yiPFpCT-eYzYpswj5CTe7_sCyLxUDqxOICZM45-8K9vNdCygvEoWtWJHKN-jxyF8ehAn9C2yHMNntlW1ahXCBA"
+})
+
+// iniciamos y creamos una tabla
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages ( 
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        user TEXT
+    )
+`)
 
 io.on('connection', (socket) => {
     console.log('User has conected')
