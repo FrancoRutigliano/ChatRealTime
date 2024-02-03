46f93499062266befa742c559132d7a3b471c10a
Franco Rutigliano <francorutigliano11@gmail.com>
2024-02-03 12:28:55 -0300

    Persistencia en los mensajes del usuario lograda

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ const db = createClient({
     authToken: process.env.DB_TOKEN
 })
 
+
 // iniciamos y creamos una tabla
 await db.execute(`
     CREATE TABLE IF NOT EXISTS messages ( 
@@ -31,17 +32,49 @@ await db.execute(`
     )
 `)
 
-io.on('connection', (socket) => {
-    console.log('User has conected')
+io.on('connection', async (socket) => {
+    console.log('un usuario se ha conectado')
 
     socket.on('disconnect', () => {
-        console.log('An user has disconnected')
+        console.log('un usuario se ha desconectado')
     })
-    // la conexion en concreto, cuando reciba el evento de 'chat message', hace algo ->>
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+
+
+    socket.on('chat message', async (msg) => {
+        let result
+        const username = socket.handshake.auth.username ?? 'anonymous'
+        console.log({ username })
+        try {
+            result = await db.execute({
+                sql: `INSERT INTO messages (content, user) VALUES (:messages, :username)`, // se utiliza para evitar sql injection
+                args: { messages: msg, username }
+            })
+        } catch (error) {
+            console.log(error)
+            return
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
     })
+    // recuperar los mensajes sin conexion, basicamente cuando estemos offline
+    if (!socket.recovered) {
+        try {
+            const results = await db.execute({
+                sql: `SELECT id, content, user FROM messages WHERE id > ?`,
+                args: [socket.handshake.auth.serverOffSet ?? 0]
+            })
+
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString())
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }
 })
+
+
+
+
 
 app.use(logger('dev'))
 
